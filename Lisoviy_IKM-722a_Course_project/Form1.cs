fbc15b829b5872156a748cb19c03f658fef526c9
Artem Lisovyi <Artem.Lisovyi@infiz.khpi.edu.ua>
2024-06-08 21:49:19 +0300

    Finish task number 1

--- Lisoviy_IKM-722a_Course_project/Form1.cs
+++ Lisoviy_IKM-722a_Course_project/Form1.cs
@@ -12,9 +12,58 @@ namespace Lisoviy_IKM_722a_Course_project
 {
     public partial class Form1 : Form
     {
+        private bool Mode;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Mode = true;
+        }
+
+        private void tClock_Tick(object sender, EventArgs e)
+        {
+            tClock.Stop();
+            MessageBox.Show("Минуло 25 секунд", "Увага");// Виведення повідомлення "Минуло 25 секунд" на екран
+tClock.Start();
+        }
+
+        private void bStart_Click(object sender, EventArgs e)
+        {
+            if (Mode)
+            {
+                tClock.Start();
+                bStart.Text = "Стоп"; // зміна тексту на кнопці на "Стоп"
+                this.Mode = false;
+                tbInput.Enabled = true;// Режим дозволу
+                tbInput.Focus();
+            }
+            else
+            {
+                tClock.Stop();
+                bStart.Text = "Пуск";// зміна тексту на кнопці на "Пуск"
+                this.Mode = true;
+                tbInput.Enabled = false;// Режим заборони введення
+            }
+        }
+
+        private void tbInput_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            tClock.Stop();
+            tClock.Start();
+            if ((e.KeyChar >= '0') & (e.KeyChar <= '9') | (e.KeyChar == (char)8))
+            {
+                return;
+            }
+            else
+            {
+                tClock.Stop();
+                MessageBox.Show("Неправильний символ", "Помилка");
+                tClock.Start();
+                e.KeyChar = (char)0;
+            }
         }
     }
 }
